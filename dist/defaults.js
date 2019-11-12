01e9982da0877686bd7f9b11049551e6a770114c
4321ip <4321ip@mail.ru>
2019-11-13 04:16:09 +0800

    Add files via upload

---
+++ dist/defaults.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MIN_WS_TIMEOUT = 1000;
+exports.MIN_WS_TIMEOUT = MIN_WS_TIMEOUT;
+var WS_TIMEOUT = 30000;
+exports.WS_TIMEOUT = WS_TIMEOUT;
+//# sourceMappingURL=defaults.js.map
